c743750b7ab698c1e13bafabe34e2dc721269f5e
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2025-04-24 20:33:41 -0600

    Refactor Navbar component: replace anchor tags with Link components for improved routing and consistency

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,21 +1,31 @@
+import Link from "next/link";
+
 const Navbar: React.FC = () => {
   return (
-    <nav aria-label="Main navigation">
-      <ul>
+    <nav
+      className="absolute top-0 left-0 right-0 flex items-center justify-between p-4"
+      aria-label="Main navigation"
+    >
+      <Link className="text-xl font-bold text-gray-800" href="/">
+        Dashboard
+      </Link>
+      <ul className="flex items-center gap-4">
         <li>
-          <a href="/">Dashboard</a>
+          <Link href="/transactions">Transacciones</Link>
         </li>
         <li>
-          <a href="/transactions">Transacciones</a>
+          <Link href="/accounts">Cuentas</Link>
         </li>
         <li>
-          <a href="/accounts">Cuentas</a>
+          <Link href="/profile">Perfil</Link>
         </li>
         <li>
-          <a href="/profile">Perfil</a>
-        </li>
-        <li>
-          <a href="/signout">Cerrar sesión</a>
+          <Link
+            className="bg-teal-600 hover:bg-teal-500 transition-colors text-white font-semibold py-2 p-4 rounded"
+            href="/signout"
+          >
+            Cerrar sesión
+          </Link>
         </li>
       </ul>
     </nav>
